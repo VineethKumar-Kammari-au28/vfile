e48a1d2a9dcdda55fe94d9cd24ce7abc6217caa6
VineethKumar-Kammari-au28 <vineethanish9@gmail.com>
2022-08-09 21:46:14 +0530

    initial

---
+++ config/db.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const mongoose = require('mongoose')
+
+const DB = "mongodb+srv://inshare:ErrorName@cluster0.byh9v.mongodb.net/inshare?retryWrites=true&w=majority"
+
+async function initMongoDB() {
+    await mongoose.connect(DB, (err) => {
+
+        if (err) {
+            console.log("Error in connecting to DB")
+        } else {
+            console.log("successfully connected to DB")
+        }
+    })
+}
+
+module.exports = initMongoDB
